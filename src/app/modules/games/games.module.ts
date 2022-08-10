00adcd87a6ca5e84088b0271b8b8703c6493e585
ChrisMatic008 <c.christmon@myemail.indwes.edu>
2022-08-10 00:53:58 -0400

    Added module

---
+++ src/app/modules/games/games.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DisplayGamesComponent } from './display-games/display-games.component';
+
+
+
+@NgModule({
+  declarations: [
+    DisplayGamesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GamesModule { }
